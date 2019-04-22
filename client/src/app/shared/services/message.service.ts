68e8d4687e2f93fdb6ae9433d1f390fe1075a0a7
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2019-04-22 18:40:42 -0400

    Fix the message's timestamp format

--- client/src/app/shared/services/message.service.ts
+++ client/src/app/shared/services/message.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import * as moment from 'moment';
 
 import { ApiService } from './api.service';
 import { distinctUntilChanged, map } from 'rxjs/operators';
@@ -14,6 +15,11 @@ export class MessageService {
       return this.apiService.get('/services/message/get-messages-by-group?groupID=' + id)
           .pipe(map(
               data => {
+                  var newMessages = data.messages.map(function(message) {
+                    message.timestamp = moment(message.timestamp).format("MM-DD-YY HH:mm:ss")
+                    return message;
+                  });
+                  data.messages = newMessages;
                   return data;
               },
               err => {
